fc3f33c53a2d7be7a31715923e701ceb9611befc
schandrabc <sharathchandrabc@gmail.com>
2026-02-16 20:10:52 -0600

    feat: [DND-1234]: Add Spring Boot Maven project structure.

---
+++ src/main/java/com/doodle/mini_doodle/MiniDoodleApiApplication.java
@@ -0,0 +1,13 @@
+package com.doodle.mini_doodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDoodleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniDoodleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doodle/mini_doodle/MiniDoodleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doodle.mini_doodle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDoodleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
